d816e0a507f52355f59c2f4cae3b3c09425d66d5
dumaaan <44192460+dumaaan@users.noreply.github.com>
2019-03-11 18:16:35 +0900

    Updated kaimarubot so that it works remotely.
    This updated is based on using webhooks instead of relying on polling method.

--- kaimaru.py
+++ kaimaru.py
@@ -1,20 +1,26 @@
-from telegram.ext import Updater, CommandHandler
-import requests
-import re
-import urllib.request as ur
+# A very simple Flask Hello World app for you to get started with...
+from flask import Flask, request
+import telepot
+import urllib3
+import urllib as ur
+import datetime
 from bs4 import BeautifulSoup
 
-updater = Updater(token='')
-dispatcher = updater.dispatcher
-import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {
+    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+}
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
 
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please send following commands: \n /breakfast - to get breakfast \n /lunch - to get lunch \n /dinner - to get dinner.")
+secret = "SECRET"
+bot = telepot.Bot('SECRET API HERE')
+bot.setWebhook("https://kaimaru.pythonanywhere.com/{}".format(secret), max_connections=1)
 
-url = "http://www.kaist.edu/_prog/fodlst/?site_dvs_cd=en&menu_dvs_cd=050303"
-html = ur.urlopen(url).read()
+
+app = Flask(__name__)
+
+surl = "http://www.kaist.edu/_prog/fodlst/?site_dvs_cd=en&menu_dvs_cd=050303"
+html = ur.request.urlopen(surl).read()
 soup = BeautifulSoup(html, features = "lxml")
 
 # kill all script and style elements
@@ -27,36 +33,33 @@ breakfast = str(menuraw[0])
 #removing unnecessary html tags, getting a cleaner return
 for r in (("<br/>","")),(("<td>","")),(("</td>","")),(("&amp;"," & ")):
     breakfast = breakfast.replace(*r)
-
 lunch = str(menuraw[1])
 #removing unnecessary html tags, getting a cleaner return
 for r in (("<br/>","")),(("<td>","")),(("</td>","")),(("&amp;"," & ")):
     lunch = lunch.replace(*r)
-
-
 dinner = str(menuraw[2])
-for r in (("<br/>","")),(("<td>","")),(("</td>","")),(("&amp;"," & ")),(('<td class="t_end">','')):
+#removing unnecessary html tags, getting a cleaner return
+for r in (("<br/>","")),(("<td class=\"t_end\">","")),(("<td>","")),(("</td>","")),(("&amp;"," & ")):
     dinner = dinner.replace(*r)
 
-def send_breakfast(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Today's for breakfast:")
-    bot.send_message(chat_id=update.message.chat_id, text=breakfast)
 
-def send_lunch(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Today's lunch options are:")
-    bot.send_message(chat_id=update.message.chat_id, text=lunch)
+@app.route('/{}'.format(secret), methods=["POST"])
+def telegram_webhook():
+    update = request.get_json()
+    if "message" in update:
+        text = update["message"]["text"]
+        chat_id = update["message"]["chat"]["id"]
+        if text == "/start":
+            bot.sendMessage(chat_id, "Hello! I'm a bot, please send following commands: \n /breakfast - to get breakfast \n /lunch - to get lunch \n /dinner - to get dinner.")
+        elif datetime.date.today().strftime("%A") == "Saturday":
+            bot.sendMessage(chat_id, "kaimaru is closed today.")
+        elif text == "/breakfast":
+            bot.sendMessage(chat_id, breakfast)
+        elif text == "/lunch":
+            bot.sendMessage(chat_id, lunch)
+        elif text == "/dinner":
+            bot.sendMessage(chat_id, dinner)
+        else:
+            bot.sendMessage(chat_id, "Ne pishi erundu")
+    return "OK"
 
-def send_dinner(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Today's dinner choices are:")
-    bot.send_message(chat_id=update.message.chat_id, text=dinner)
-
-
-start_handler = CommandHandler('start', start)
-bf_handler = CommandHandler('breakfast', send_breakfast)
-ln_handler = CommandHandler('lunch', send_lunch)
-dn_handler = CommandHandler('dinner', send_dinner)
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(bf_handler)
-dispatcher.add_handler(ln_handler)
-dispatcher.add_handler(dn_handler)
-updater.start_polling()
